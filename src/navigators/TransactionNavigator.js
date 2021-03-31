c1f793d0618a96ef9f831a45964d8aba038449e0
hau28 <muiheoconghau@gmail.com>
2021-03-31 12:02:59 +0700

    Refactor some basic components

--- src/navigators/TransactionNavigator.js
+++ src/navigators/TransactionNavigator.js
@@ -1,28 +1,24 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import React, { Component } from "react";
-import {
-    TransactionScreen,
-    EditTransactionScreen,
-}
-from "../screens"
+import { TransactionScreen, EditTransactionScreen } from "../screens";
 
 const TransactionStack = createStackNavigator();
 
 export default class TransactionNavigator extends Component {
-    render() {
-        return (
-            <TransactionStack.Navigator>
-                <TransactionStack.Screen
-                    name="TransactionScreen"
-                    component={TransactionScreen}
-                    options={{ headerShown: false }}
-                />
-                <TransactionStack.Screen
-                    name="EditTransaction"
-                    component={EditTransactionScreen}
-                    options={{ headerShown: true, title: "Chỉnh sửa giao dịch" }}
-                />
-            </TransactionStack.Navigator>
-        );
-    }
+  render() {
+    return (
+      <TransactionStack.Navigator>
+        <TransactionStack.Screen
+          name="TransactionScreen"
+          component={TransactionScreen}
+          options={{ headerShown: false }}
+        />
+        <TransactionStack.Screen
+          name="EditTransaction"
+          component={EditTransactionScreen}
+          options={{ headerShown: true, title: "Chỉnh sửa giao dịch" }}
+        />
+      </TransactionStack.Navigator>
+    );
+  }
 }
