e5e7277080daf6d6befddc01d68cc667a03d5fcf
LaNeitD18 <phanhuytien14@gmail.com>
2021-04-14 08:54:11 +0700

    Commit to pull

--- src/screens/transaction/TransactionScreen.js
+++ src/screens/transaction/TransactionScreen.js
@@ -5,7 +5,7 @@ export default class TransactionScreen extends React.Component {
   render() {
     return (
       <View>
-        <Text style={{ fontSize: 20 }} />
+        <Text style={{ fontSize: 20 }}>TransactionScreen</Text>
       </View>
     );
   }
