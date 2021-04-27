abd49e6711365d4e5e2efd71c12ce7a53c493a91
hau28 <muiheoconghau@gmail.com>
2021-04-27 16:20:21 +0700

    Remove react-native-gesture-handler

--- src/screens/transaction/TransactionScreen.js
+++ src/screens/transaction/TransactionScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View } from "react-native";
+import { View, FlatList } from "react-native";
 import {
   ScreenView,
   TransactionMonthSummary,
@@ -19,7 +19,6 @@ import {
 import * as firebase from "firebase";
 import { findIcon } from "../../components/Image";
 
-import { FlatList } from "react-native-gesture-handler";
 import toMoneyString from "../../components/toMoneyString";
 import { userRef } from "../../components/DataConnect";
 
